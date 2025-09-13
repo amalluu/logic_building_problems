e1ec3a25b84e7103bb6449972b13f5497ce88295
Amalu Kuruvilla <amalukuruvilla1234@gmail.com>
2025-09-14 00:10:45 +0530

    Find the maximum and minimum elements in an array

---
+++ array_problems/maxmin.py
@@ -0,0 +1,11 @@
+#Find the maximum and minimum elements in an array.
+
+
+arr = list(map(int,input("Enter numbers separated by space: ").split()))
+n= len(arr)
+
+min= min(arr)
+max= max(arr)
+
+print(min)
+print(max)
