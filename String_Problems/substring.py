2b052e421b2d72112fc5aa506b4b294a2a5df276
Amalu Kuruvilla <amalukuruvilla1234@gmail.com>
2025-09-21 10:43:44 +0530

    find all substrings of a given string

---
+++ String_Problems/substring.py
@@ -0,0 +1,9 @@
+#find all substrings of a given string
+str= input("Enter the String: ")
+sub=[]
+n=len(str)
+for i in range(n):
+    for j in range(i+1,n+1):# runs from i+1 up to n (Python slicing excludes the end index → n+1)
+        sub.append(str[i:j])# take substring from index i to j-1 and add to list
+
+print(f"All substrings: {sub}")
