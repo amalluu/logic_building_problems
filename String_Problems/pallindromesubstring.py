444873f4f579b0fd6e3dd185431272340e426faf
Amalu Kuruvilla <amalukuruvilla1234@gmail.com>
2025-09-21 11:05:28 +0530

    find all pallindrome substrings in a string

---
+++ String_Problems/pallindromesubstring.py
@@ -0,0 +1,20 @@
+#Find palindromic substrings in a string
+
+#Substrings
+str= input("Enter the String: ")
+sub=[]
+n=len(str)
+for i in range(n):
+    for j in range(i+1,n+1):# runs from i+1 up to n (Python slicing excludes the end index → n+1)
+        sub.append(str[i:j])# take substring from index i to j-1 and add to list
+print(f"All substrings: {sub}")
+
+# Find pallindrome substrings
+pallindromes=[]
+for substring in sub:
+   
+    if substring[::-1]==substring:
+        pallindromes.append(substring)
+print(f"Pallindrome substrings are: {pallindromes}")
+
+
